969b61d24091ea04b0fe8d1f9f3b5b0a2d7de2a5
HaimarAr <hai_ari_mor@goierrieskola.org>
2025-01-14 16:42:26 +0100

    PAntalla guztiak eginda baina DB ra konektatzea falta da, Insert-ak falta dira eta prezio totala kalkulatu.

---
+++ app/src/main/java/com/example/zerbitzariapp/Produktua.kt
@@ -0,0 +1,13 @@
+package com.example.zerbitzariapp
+
+data class Produktua(
+    val id: Int,
+    val izena: String,
+    val deskribapena: String,
+    val prezioa: Float,
+    val erosketaPrezioa: Float,
+    val kantitatea: Int,
+    val kantitateMinimoa: Int,
+    val mota: Int
+)
+
